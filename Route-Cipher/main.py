66cc0c0b58cb674da519e93eb01d79caf1c32fe9
Waranu <usmanhaneef43@gmail.com>
2023-06-03 07:03:28 +0500

    Completed the Console Prototype

--- Route-Cipher/main.py
+++ Route-Cipher/main.py
@@ -1,10 +1,10 @@
 from random import randint
+from time import sleep
 
-# Rules of Game of life 
-# Rule 1: Any cell with <= 2 or 3 neighbours dies
-# Rule 2: Any cell with  >= 4 neighbours dies 
-# Rule 3: Any dead cell with exactly 3 neighbours comes alive
-# 0 = Dead Cell and 1 = Alive Cell
+# Any live cell with < 2 live numNeighbours dies, as if by underpopulation.
+# Any live cell with 2 <= x <= 3 live numNeighbours lives on to the next generation.
+# Any live cell with > 3 numNeighbours dies, as if by overpopulation.
+# Any dead cell with = 3 three live numNeighbours becomes a live cell, as if by reproduction.
 
 
 
@@ -17,21 +17,87 @@ BOARD = [[0 for j in range(COLS)] for x in range(ROWS)]
 # TODO: Popularize the board via the random function
 
 def FillboardRandomly(board):
-     for i in range(ROWS):
-          for j in range(COLS):
-               board[i][j] = randint(0, 1)
+     randomRow = randint(0, 3)
+     randomColumn = randint(0, 3)
+     
+     
+     for i in range(ROWS-3):
+          for j in range(COLS-3):
+               board[i+randomRow][j+randomColumn] = randint(0, 1)
 
 def Printboard(board):
      for i in board:
           print(i)
+     print("\n")
 
+# TODO: Try the compute intensive approach
+     # TODO: Code the basic Rules stated above
+
+
+
+
+def CreateNextState(board):
+     numNeighbours = 0
+     UP = 0 
+     DOWN = 0
+     LEFT = 0
+     RIGHT = 0
+     
+     for x in range(ROWS):
+          for y in range(COLS):
+               #FIXME: What to do when x or y = 0 ? 
+               LEFT = (x - 1) % ROWS
+               RIGHT = (x + 1) % ROWS
+               UP = (y - 1) % COLS
+               DOWN = (y + 1) % COLS
+               
+               # Check for numNeighbours
+               
+               if board[x][UP] == 1:
+                    numNeighbours += 1
+                         
+               if board[x][DOWN] == 1:
+                    numNeighbours += 1
+                         
+               if board[LEFT][UP] == 1:
+                    numNeighbours += 1
+                         
+               if board[LEFT][DOWN] == 1:
+                    numNeighbours += 1
+                         
+               if board[RIGHT][UP] == 1:
+                    numNeighbours += 1
+                              
+               if board[RIGHT][DOWN] == 1:
+                    numNeighbours += 1
+                              
+               if board[LEFT][y] == 1:
+                    numNeighbours += 1
+                              
+               if board[RIGHT][y] == 1:
+                    numNeighbours += 1
+                
+               if numNeighbours > 0:
+                    if (numNeighbours == 2 or numNeighbours == 3) and board[x][y] == 1:
+                         board[x][y] = 1
+                    elif numNeighbours == 3 and board[x][y] == 0:
+                         board[x][y] = 1
+                    else: 
+                         board[x][y] = 0
+                         numNeighbours = 0     
+     sleep(1.0)
 
 
 
 def main():
      FillboardRandomly(BOARD)
      Printboard(BOARD)
-
+     
+     run = True
+     
+     while run:
+          CreateNextState(BOARD)
+          Printboard(BOARD)
 
 
 
